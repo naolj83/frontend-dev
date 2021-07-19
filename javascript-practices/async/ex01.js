7257e2d5650809a8160b2ad4636e3cdda543a7fd
naolj83 <naolj83@gmail.com>
2021-07-19 14:55:23 +0900

    비동기 테스트 #2

--- javascript-practices/async/ex01.js
+++ javascript-practices/async/ex01.js
@@ -1,35 +1,35 @@
-const { REPL_MODE_SLOPPY } = require("repl");
-
 const ex01 = function(param, callback){
     // 비동기 코드
-    // ex) 파일 시스템 접근, 네트워크 통신, SQL to DB, setTimeout
+    // ex) 파일시스템 접근, 네트워크 통신, SQL to DB, setTimeout
     // callback 방식, promise 방식
-
     setTimeout(function(){
-        if(param == 'param-data'){
+        if(param === 'param-data') {
             callback(null, 'ok');
         } else {
-            callback(new Error('fail'));
+            callback(new Error('fail'), null);
         }
-    }, 2000);
+    }, 1000);
 }
 
-// test01
-ex01("param-data", function(err, res){
-    if(err) {
-        console.error(err);
-    } else {
-        console.log(res);
-    }
-});
+if(require.main == module) {
+    // test01
+    ex01("param-data", function(err, res){
+        if(err){
+            console.error(err);
+        } else {
+            console.log(res);
+        }
+    });
 
-// test02
-ex01("param-error", function(err, res){
-    if(err) {
-        console.error(err.message);
-    } else {
-        console.log(res);
-    }
-});
-
-console.log("waits....")
+    // test02
+    ex01("param-error", function(err, res){
+        if(err){
+            console.error(err.message);
+        } else {
+            console.log(res);
+        }
+    });
+    console.log("waits....");
+} else {
+    module.exports = ex01;
+}
